b708ec2dd666992b2c96177a020bec3134712c74
Henrique Junqueira <41648535+henriquejunqueira@users.noreply.github.com>
2025-01-13 13:22:07 -0300

    Expressões condicionais

---
+++ modulo01/aula03/script.js
@@ -0,0 +1,20 @@
+// ? expressões condicionais
+
+const grupoA = 100;
+const grupoB = 200;
+
+// ! if-else
+// if (grupoA > grupoB) {
+//   console.log('Grupo A, ganhou');
+// } else {
+//   console.log('Grupo B, ganhou');
+// }
+
+// ! ternário
+// const vencedor = grupoA > grupoB ? 'Grupo A Venceu' : 'Grupo B Venceu';
+
+const active = true;
+const button = active && 'Botão está ativo';
+
+// console.log(vencedor);
+console.log(button);
